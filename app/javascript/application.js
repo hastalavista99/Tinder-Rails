954dbc17a5007831d0598e78e29ddd21e59500b1
hastalavista <githumuj@gmail.com>
2023-08-17 16:38:17 +0300

    added icons and bootstrap styling
    also added migration for local storage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,29 @@
 // Entry point for the build script in your package.json
+document.addEventListener("DOMContentLoaded", function () {
+
+    let slides = document.querySelectorAll('.slide');
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        slides.forEach(function(slide, i) {
+            slide.classList.remove('showing');
+            if (i === index) {
+                slide.classList.add('showing');
+            }
+        });
+    }
+
+    document.getElementById('approve').addEventListener('click', function() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    });
+
+    document.getElementById('decline').addEventListener('click', function() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    });
+
+
+
+
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Account.create([
+  { first_name: "Sarah", last_name: "Jones", username: "sarahj", email: "sarah@example.com", password: "reminisce" },
+  { first_name: "Emily", last_name: "Hart", username: "emily", email: "emily@example.com", password: "reminisce" },
+  { first_name: "Melanie", last_name: "Hunt", username: "melanie", email: "melanie@example.com", password: "reminisce" },
+  { first_name: "Caroline", last_name: "Simpson", username: "caroline", email: "caroline@example.com", password: "reminisce" },
+  { first_name: "Bridget", last_name: "Smith", username: "bridget", email: "bridget@example.com", password: "reminisce" },
+])
